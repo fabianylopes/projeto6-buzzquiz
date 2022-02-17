eff095d2618d1918cc2f36a9756d6dae6817f928
JuliannaRkg544 <juliana.rkg544@gmail.com>
2022-02-17 10:00:08 -0300

    validações das informações básicas ok

--- js/scriptTela3.js
+++ js/scriptTela3.js
@@ -1,28 +1,46 @@
 const BUZZQUIZZ_API = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
 
 
-function YourQuizInfo(){
+function YourQuizInfo() {
     let tela3 = document.querySelector('.your-quiz');
     tela3.classList.remove('hidden')
-   const yourQuizzTittle = document.querySelector('#your-quiz-tittle').value;
-   yourQuizzTittle.innerHTML = ` 
+    const yourQuizzTittle = document.querySelector('#your-quiz-tittle').value;
+    yourQuizzTittle.innerHTML = ` 
    <input type="text" id="your-quiz-tittle" placeholder="Título do seu quiz">
    
    `
-
     const yourQuizzURL = document.querySelector('#your-quiz-URL').value;
     yourQuizzURL.innerHTML = ` 
     <input type="text" id="your-quiz-URL" placeholder="URL da imagem do seu quiz"">
     
     `
     const yourQuizzQuestions = document.querySelector('#your-quiz-questions').value;
-   // yourQuizzQuestions.innerHTML=`
-   // <input type="text" id="your-quiz-questions" placeholder="Quantidade de perguntas do quizz" onclick="YourQuizInfo()">
-  //</input>  `;
+    yourQuizzQuestions.innerHTML = `
+    <input type="text" id="your-quiz-questions" placeholder="Quantidade de perguntas do quizz">
+  `;
     const yourQuizzLevels = document.querySelector('#your-quiz-levels').value;
-   // yourQuizzLevels.innerHTML=" ";
+    yourQuizzLevels.innerHTML = `
+    <input type="text" id="your-quiz-levels" placeholder="Quantidade de níveis do quiz">
+  `;
+    if (yourQuizzTittle.length < 20 || yourQuizzTittle.length > 65) {
+        alert("o título do seu quiz deve ter entre 20 e 65 carateres!!");
 
-  
+    }
+    if (Number(yourQuizzQuestions) < 3) {
+        alert("você deve inserir no mínimo 3 perguntas!!");
+
+    }
+    if (Number(yourQuizzLevels) < 2) {
+        alert("o quiz deve ter no mínimo 2 levels!!");
+
+    }
+    if (yourQuizzURL.includes('http')===false) {
+        alert("a imagem deve ter um formato URL!!");
+
+    }    
+
+    
+
     let objectYourQuiz = {
         title: yourQuizzTittle,
         image: yourQuizzURL,
@@ -90,11 +108,11 @@ function YourQuizInfo(){
                 minValue: 50
             }
         ]
-    
+
     }
-    
+
     const promise = axios.post(BUZZQUIZZ_API, objectYourQuiz)
-    
+
     promise.then(createQuiz);
     promise.catch(createQuizError);
 
@@ -102,13 +120,13 @@ function YourQuizInfo(){
 
 YourQuizInfo();
 
-function createQuiz(resposta){
+function createQuiz(resposta) {
     console.log(resposta.data);
     console.log("vamos la, seguindo em frente");
 
 }
 
 
-function createQuizError(error){
+function createQuizError(error) {
 
 }
