b3213fae484b90410033cc2c7b179b4d765a0823
JuliannaRkg544 <juliana.rkg544@gmail.com>
2022-02-17 17:49:56 -0300

    trabalhando na transição entre telas

--- js/scriptTela3.js
+++ js/scriptTela3.js
@@ -1,47 +1,22 @@
 const BUZZQUIZZ_API = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
 
 
+
 function YourQuizInfo() {
-    let tela3 = document.querySelector('.your-quiz');
-    tela3.classList.remove('hidden')
-    const yourQuizzTittle = document.querySelector('#your-quiz-tittle').value;
-    yourQuizzTittle.innerHTML = ` 
-   <input type="text" id="your-quiz-tittle" placeholder="Título do seu quiz">
-   
-   `
-    const yourQuizzURL = document.querySelector('#your-quiz-URL').value;
-    yourQuizzURL.innerHTML = ` 
-    <input type="text" id="your-quiz-URL" placeholder="URL da imagem do seu quiz"">
-    
-    `
-    const yourQuizzQuestions = document.querySelector('#your-quiz-questions').value;
-    yourQuizzQuestions.innerHTML = `
-    <input type="text" id="your-quiz-questions" placeholder="Quantidade de perguntas do quizz">
-  `;
-    const yourQuizzLevels = document.querySelector('#your-quiz-levels').value;
-    yourQuizzLevels.innerHTML = `
-    <input type="text" id="your-quiz-levels" placeholder="Quantidade de níveis do quiz">
-  `;
-    if (yourQuizzTittle.length < 20 || yourQuizzTittle.length > 65) {
-        alert("o título do seu quiz deve ter entre 20 e 65 carateres!!");
+    let infoTela3 = document.querySelector('.your-quiz');
+    //infoTela3.classList.remove('.hidden')
 
-    }
-    if (Number(yourQuizzQuestions) < 3) {
-        alert("você deve inserir no mínimo 3 perguntas!!");
+    let yourQuizzTittle = document.querySelector('#your-quiz-tittle').value;
+ 
+    let yourQuizzURL = document.querySelector('#your-quiz-URL').value;
 
-    }
-    if (Number(yourQuizzLevels) < 2) {
-        alert("o quiz deve ter no mínimo 2 levels!!");
+    let yourQuizzQuestions = document.querySelector('#your-quiz-questions').value;
 
-    }
-    if (yourQuizzURL.includes('http')===false) {
-        alert("a imagem deve ter um formato URL!!");
+    let yourQuizzLevels = document.querySelector('#your-quiz-levels').value;
 
-    }    
-
-    
 
     let objectYourQuiz = {
+
         title: yourQuizzTittle,
         image: yourQuizzURL,
         questions: [
@@ -111,22 +86,87 @@ function YourQuizInfo() {
 
     }
 
+
+    if (yourQuizzTittle.length < 20 || yourQuizzTittle.length > 65) {
+        alert("o título do seu quiz deve ter entre 20 e 65 carateres!!");
+       // window.location.reload()
+        document.querySelector("#your-quiz-tittle").value = ""
+
+    }
+    if (Number(yourQuizzQuestions) < 3) {
+        alert("você deve inserir no mínimo 3 perguntas!!");
+       // window.location.reload()
+        document.querySelector("#your-quiz-questions").value = ""
+    }
+    if (Number(yourQuizzLevels) < 2) {
+        alert("o quiz deve ter no mínimo 2 levels!!");
+      //  window.location.reload()
+        document.querySelector("#your-quiz-levels").value = ""
+    }
+    if (yourQuizzURL.includes('http') === false) {
+        alert("a imagem deve ter um formato URL!!");
+       // window.location.reload()
+        document.querySelector("#your-quiz-URL").value = ""
+    }
+
     const promise = axios.post(BUZZQUIZZ_API, objectYourQuiz)
 
-    promise.then(createQuiz);
-    promise.catch(createQuizError);
-
-}
-
-YourQuizInfo();
-
-function createQuiz(resposta) {
+    promise.then(function(resposta){
     console.log(resposta.data);
     console.log("vamos la, seguindo em frente");
+    })
+    promise.catch(createQuizError);
+
+
 
 }
+
+
+
+
+function yourQuizzLevel(resposta) {
+
+   
+    let levelTela3 = document.querySelector('.your-quiz-levels');
+    levelTela3.classList.remove('hidden');
+    
+
+    let yourQuizzLevelTittle = document.querySelector('#your-quiz-level-tittle').value;
+    
+    let yourQuizzLevelPercent = document.querySelector('#your-quiz-level-percent').value;
+   
+    let yourQuizzLevelURL = document.querySelector('#your-quiz-level-URL').value;
+
+    let yourQuizzLevelText = document.querySelector('#your-quiz-level-text').value;
+    
+    if (yourQuizzLevelTittle.length < 10) {
+        alert("o título do nível do seu quiz deve ter no mínimo 10 caracteres!!");
+        window.location.reload()
+
+    }
+    // if (Number(yourQuizzLevelPercent) < 3) {
+    //    alert("você deve inserir no mínimo 3 perguntas!!");
+    //    window.location.reload()
+    //}
+
+ if (yourQuizzLevelURL.includes('http') === false) {
+        alert("a imagem deve ter um formato URL!!");
+        window.location.reload()
+    }
+
+    if  (yourQuizzLevelText.length < 30) {
+        alert("a descrição do quiz deve ter no mínimo 30 caracteres!!");
+        window.location.reload()
+    }
+   
+
+}
+
 
 
 function createQuizError(error) {
 
 }
+
+
+
