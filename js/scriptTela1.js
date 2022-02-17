fb528822d4805936495f9e73ee8a036bfae4ca65
Faby <fabiany.lopes.060@ufrn.edu.br>
2022-02-17 12:09:46 -0300

    interação entre telas

--- js/scriptTela1.js
+++ js/scriptTela1.js
@@ -1,12 +1,17 @@
 
-// function choseQuizz(){   
-//     const secondScreen = document.querySelector('.quiz-open');
-//     secondScreen.classList.remove('hidden');
+function userQuizzes(){
+    
+}
 
-//     const firstScreen = document.querySelector('.screen-one');
-//     firstScreen.classList.add('hidden');
+function quizzesCreated(){
+    const userQuizz = document.querySelector('.created-quizzes');
+    userQuizz.classList.remove('hidden');
+}
 
-// }
+function noQuizzes(){
+    const userQuizz = document.querySelector('.create-quizz');
+    userQuizz.classList.remove('hidden');
+}
 
 getQuizzes();
 
@@ -21,16 +26,43 @@ function quizzesList(response){
 
     let info = response.data;
 
-    console.log(info);
-
     for(let i = 0; i < info.length; i++){
         quizzes.innerHTML += `
-        <div class="quizzes-list">
-            <img src="${info.image}">
+        <div class="quizzes-list" onclick="showQuizz(${info[i].id})">
+            <img src="${info[i].image}">
             <div class="overlay"></div>
-            <div class="title">${info.title}</div>
+            <div class="title">${info[i].title}</div>
         </div>
         `
-
     }
+}
+
+function showQuizz(id){
+
+    const promise = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`);
+    promise.then(success);
+
+    choseQuizz();
+
+}
+
+function choseQuizz(){   
+    const screenTwo = document.querySelector('.quiz-open');
+    screenTwo.classList.remove('hidden');
+
+    const screenOne = document.querySelector('.screen-one');
+    screenOne.classList.add('hidden');
+
+}
+
+function success(response){
+    console.log(response)
+}
+
+function createQuizz(){
+    const screenThree = document.querySelector('.your-quiz-basic-info');
+    screenThree.classList.remove('hidden');
+
+    const screenOne = document.querySelector('.screen-one');
+    screenOne.classList.add('hidden');
 }
